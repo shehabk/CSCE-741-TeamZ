07a954ad988b7505bf39c9e077a0b2a296a59376
Ahmed Shehab Khan <the.shihab.khan@gmail.com>
2017-12-08 11:17:53 -0500

    updated pom.xml to have Junit 4.12

--- src/test/java/io/teamz/course/CourseServiceTest.java
+++ src/test/java/io/teamz/course/CourseServiceTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
